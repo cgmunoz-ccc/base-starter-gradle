db944ac51646512c4f9b375a2434209b6d382744
Carmen García <cgmunoz@copyright.com>
2025-08-18 11:57:48 +0200

    Remove duplicated code

--- src/main/java/com/vaadin/starter/skeleton/views/BreadCrumb.java
+++ src/main/java/com/vaadin/starter/skeleton/views/BreadCrumb.java
@@ -22,40 +22,23 @@ public class BreadCrumb extends HorizontalLayout {
     }
 
     private void addBreadcrumbButtons(){
-        Button buttonUserDataView = new Button("Your Data");
-        buttonUserDataView.addClickListener(e ->
-                buttonUserDataView.getUI().ifPresent(ui ->
-                    ui.navigate("calculator")));
-        buttonUserDataView.addClassName("visibleBtn");
-
-        Button buttonCalculatorView = new Button("Calculator");
-        buttonCalculatorView.addClassName("visibleBtn");
-        buttonCalculatorView.addClickListener(e ->
-            buttonCalculatorView.getUI().ifPresent(ui ->
-                ui.navigate("calculator/start")));
-
-        Button buttonSummaryView = new Button("Summary");
-        buttonSummaryView.addClickListener(e ->
-            buttonSummaryView.getUI().ifPresent(ui ->
-                ui.navigate("calculator/summary")));
-        buttonSummaryView.addClassName("visibleBtn");
+        Button buttonUserDataView = createBreadcrumbButton(
+            "Your Data", "calculator");
+        Button buttonCalculatorView = createBreadcrumbButton(
+            "Calculator", "calculator/start");
+        Button buttonSummaryView = createBreadcrumbButton(
+            "Summary", "calculator/summary");
 
         add(buttonUserDataView, buttonCalculatorView, buttonSummaryView);
     }
 
-//    static void goToView(final UI ui,
-//                         final Class<? extends View> classView) {
-//        ui.getPage().executeJs(SCROLL_UP);
-//        ui.navigate(classView);
-//    }
+    private Button createBreadcrumbButton(String buttonLabel, String buttonRoute){
+        Button button = new Button(buttonLabel);
+        button.addClassName("visibleBtn");
 
-//    private Button createBreadcrumbButton(final Class<T> classView, final String buttonLabel){
-//        final Button button = new Button(buttonLabel,
-//            buttonClickEvent -> UI.getCurrent().getUI()
-//                .ifPresent(ui -> NavigationButtonLayout.goToView(ui, classView, Optional.empty())));
-//        button.removeClassNames(VISIBLE, HIDDEN);
-//        button.addClassName(HIDDEN);
-//        return button;
-//    }
-//    }
+        button.addClickListener(e ->
+            button.getUI().ifPresent(ui ->
+                ui.navigate(buttonRoute)));
+        return button;
+    }
 }
